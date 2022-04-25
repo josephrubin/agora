673633c15d72427e8779082e58d5ac50f6aae9be
Jorge Zreik <jfzreik@gmail.com>
2022-04-25 17:28:07 -0400

    Redirect from index to login and home

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,7 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import { LoaderFunction, redirect } from "remix";
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
+// https://remix.run/docs/en/v1/api/conventions#loader
+export const loader: LoaderFunction = async () => {
+  const isLoggedIn = true;
+  return isLoggedIn ? redirect("/login") : redirect("home");
 };
-
-// https://remix.run/api/conventions#meta
-export const meta: MetaFunction = () => {
-  return {
-    title: "Remix Starter",
-    description: "Welcome to remix!",
-  };
-};
-
-// https://remix.run/guides/routing#index-routes
-export default function Index() {
-  const data = useLoaderData<IndexData>();
-
-  return (
-    <div className="remix__page">
-      <main>
-        <h2>Welcome to RoJo App!</h2>
-        <p>It&apos;s gonna be a good time 🥳</p>
-        <p>
-          This app makes it easy for communities to create NFT collections and
-          award them to users.
-        </p>
-      </main>
-    </div>
-  );
-}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,40 +1,39 @@
-import { ActionFunction, Form, redirect, useActionData } from "remix";
+import { ActionFunction, Form, useActionData } from "remix";
 import { createSession, createSessionRedirect } from "~/modules/users.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
 
-  const username = String(formData.get("username")!);
-  const password = String(formData.get("password")!);
+  // TODO: form validation
+  const username = String(formData.get("username"));
+  const password = String(formData.get("password"));
 
   // For now we assume no errors. TODO - fix this.
 
-  const session = await createSession({username: username, password: password});
+  const session = await createSession({
+    username: username,
+    password: password,
+  });
 
   if (session && session.accessToken) {
     return createSessionRedirect(session, "/");
-  }
-  else {
+  } else {
     return {
       error: "Could not make session",
     };
   }
 };
 
-export default function Register() {
+export default function Login() {
   const actionData = useActionData();
 
   if (actionData && actionData.error) {
-    return (
-      <p>
-        {actionData.error}
-      </p>
-    );
+    return <p>{actionData.error}</p>;
   }
 
   return (
     <div>
-      <h1>Sign In</h1>
+      <h1>Agora</h1>
       <Form method="post">
         <label>
           Username
@@ -45,7 +44,8 @@ export default function Register() {
           Password
           <input name="password" type="password" />
         </label>
-        <br /><br />
+        <br />
+        <br />
         <input type="submit" value="Start session" />
       </Form>
     </div>
