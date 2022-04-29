fdb958054e983a1e5ba191621fe606eb181df492
Aryan Bhasin <bhasinaryan@gmail.com>
2022-04-29 11:37:37 -0400

    Adds scaffolding (type definitions + dummy data) for Metaplex NFT

--- app/components/mint.tsx
+++ app/components/mint.tsx
@@ -3,6 +3,7 @@ import { useWallet, useConnection } from  "@solana/wallet-adapter-react";
 import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
 
 import { mintNFT } from "../actions/mintNFT";
+import { Data, CreatorClass } from "~/types";
 
 export default function Mint() {
   const { connection } = useConnection();
@@ -10,6 +11,27 @@ export default function Mint() {
 
   const mint = async() => {
     if (!wallet.publicKey) throw new WalletNotConnectedError();
+
+    // ------ TODO: Right now, we mint the same NFT of a cat every time this function is called -------------
+    const creator = new CreatorClass({
+      address: wallet?.publicKey?.toBase58(),
+      share: 100,
+      verified: 1,
+    });
+
+    // A sample URI that points to a JSON file following Metaplex Token Metadata standard.
+    const DUMMY_ARWEAVE_METADATA_URI = "https://ad46wdl5rjjowlu4yad7dxh3b2xfi7nyreudj2zzqt3dzuzob4.arweave.net/APnrDX2KUusunMAH8dz7Dq5UfbiJKDTrOYT2-PNMuDw/";
+
+    const data = new Data({
+      symbol: "CAT",
+      name: "Cat!",
+      uri: DUMMY_ARWEAVE_METADATA_URI,
+      sellerFeeBasisPoints: 0,
+      creators: [creator],
+    });
+    console.log(data);
+
+    // ------ TODO: Right now, we mint the same NFT of a cat every time this function is called -------------
 
     try {
       mintNFT();
@@ -39,7 +61,7 @@ const LaunchSection = (props: {
         name="mint"
         htmlType="submit"
         size="large"
-        style={{ padding: 10, background: "blue" }}
+        style={{ padding: 10 }}
         onClick={() => {
           props.mint();
         }}
