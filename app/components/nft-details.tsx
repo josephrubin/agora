aa37316952db3990fd6f2502bd5f6fb9cce20935
Jorge Zreik <jfzreik@gmail.com>
2022-05-03 17:05:52 -0400

    Home image load spinners

--- app/components/nft-details.tsx
+++ app/components/nft-details.tsx
@@ -1,5 +1,5 @@
 // Adds Solana wallet context. See https://github.com/solana-labs/wallet-adapter
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Form } from "remix";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
@@ -21,6 +21,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 
 import ExportNFT from "~/components/export-nft";
 import { HistoryItem } from "~/generated/graphql-schema";
+import { Spinner } from "./spinner";
 
 function NFTDetails(props: {
   id: string,
@@ -70,13 +71,16 @@ const NftDetailsView = (props: {
   imageUri: string,
   history: HistoryItem[]
 }) => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div>
       <h1 className="mb-4">{props.title || "NFT Details"}</h1>
       <div className="flex flex-row gap-8 space-between">
         <div className="flex flex-col gap-8">
           <div className="flex flex-col items-center justify-center flex-shrink-0 border-2 rounded-md w-96 h-72">
-            <img className="object-contain w-full h-full" src={props.imageUri} />
+            <img className="z-10 object-contain w-full h-full" src={props.imageUri} onLoad={() => setLoaded(true)} />
+            {!loaded ? <div className="absolute mx-auto my-auto"><Spinner /></div> : null}
           </div>
         </div>
         <div className="flex flex-col w-full gap-2">
@@ -88,6 +92,7 @@ const NftDetailsView = (props: {
           <hr className="my-2" />
           <h2>Transaction History</h2>
           <ol className="h-16 ml-8 overflow-y-auto">
+            {props.history.length === 0 ? <li>Minted!</li> : null}
             {props.history?.map((h, i) =>  <li key={i}>
               ({h.epoch}) &#8594; {h.target}</li>)
             }
